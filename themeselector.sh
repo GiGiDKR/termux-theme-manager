b7f6b4c5a1c15ff4a83eecaccdefd14723ea02f3
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-07-26 00:51:42 +0200

    Update themeselector.sh

--- themeselector.sh
+++ themeselector.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-THEMES_DIR="$HOME/.termux"
+# Définition des variables
 TERMUX_COLORS="$HOME/.termux/colors.properties"
-BACKUP_COLORS="$HOME/.termux/colors.properties.backup"
+THEMES_DIR="$HOME/.termux/themes"
 
 # Fonction pour afficher la bannière
 banner() {
@@ -38,49 +38,14 @@ banner() {
     echo ""
 }
 
-# Fonction pour télécharger et extraire les thèmes
-telecharger_themes() {
-    echo "Téléchargement des thèmes..."
-    curl -L -o termux.zip https://github.com/GiGiDKR/termux-theme-manager/raw/main/termux.zip
-    unzip -o termux.zip -d "$THEMES_DIR"
-    rm termux.zip
-    echo "Thèmes téléchargés et extraits avec succès."
-}
-
-# Vérifier et installer curl et unzip si nécessaire
-if ! command -v curl &> /dev/null || ! command -v unzip &> /dev/null; then
-    echo "Installation des dépendances nécessaires..."
-    pkg update
-    pkg install -y curl unzip
-fi
-
-banner
-
-# Vérifier si les thèmes existent déjà
-if [ ! "$(ls -A "$THEMES_DIR"/*.properties 2>/dev/null)" ]; then
-    echo "Aucun thème trouvé. Téléchargement des thèmes..."
-    telecharger_themes
-fi
-
-if [ -f "$TERMUX_COLORS" ]; then
-    cp "$TERMUX_COLORS" "$BACKUP_COLORS"
-    echo ""
-    echo "Thème initial sauvegardé dans $BACKUP_COLORS"
-else
-    echo ""
-    echo "Aucun thème initial trouvé. Création d'un nouveau."
-    touch "$TERMUX_COLORS"
-fi
-
 # Fonction pour appliquer un thème
 appliquer_theme() {
     local theme_file="$1"
     local theme_name="$2"
-    
+
     cp "$theme_file" "$TERMUX_COLORS"
-    
     termux-reload-settings
-    
+
     clear
     banner
     echo "                        ${theme_name^^}"
@@ -97,6 +62,21 @@ appliquer_theme() {
     fi
 }
 
+# Vérifier si le dossier des thèmes existe, sinon le créer
+if [ ! -d "$THEMES_DIR" ]; then
+    mkdir -p "$THEMES_DIR"
+fi
+
+# Télécharger et extraire les thèmes si le dossier est vide
+if [ -z "$(ls -A "$THEMES_DIR")" ]; then
+    echo "Téléchargement des thèmes..."
+    curl -L -o termux.zip https://github.com/GiGiDKR/termux-theme-manager/raw/main/termux.zip
+    unzip -o termux.zip -d "$THEMES_DIR"
+    rm termux.zip
+    echo "Thèmes téléchargés et extraits."
+fi
+
+# Parcourir et appliquer les thèmes
 for theme_file in "$THEMES_DIR"/*.properties; do
     theme_name=$(basename "$theme_file" .properties)
     appliquer_theme "$theme_file" "$theme_name"
@@ -106,16 +86,3 @@ clear
 banner
 echo ""
 echo "Tous les thèmes ont été parcourus sans qu'aucun ne soit choisi."
-
-if [ -f "$BACKUP_COLORS" ]; then
-    cp "$BACKUP_COLORS" "$TERMUX_COLORS"
-    termux-reload-settings
-    echo ""
-    echo "     Le thème initial a été restauré automatiquement."
-else
-    echo ""
-    echo "Aucune sauvegarde de thème trouvée. Le dernier a été conservé."
-fi
-
-echo ""
-echo "                    Au revoir !"
